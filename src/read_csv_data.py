8f5e29316564670f34f1df73631d93eee15630c4
dpatrik0630 <d.patrik0630@gmail.com>
2021-07-01 08:32:34 +0200

    feat: implemented required csv reader function with Pandas Dataframe format issues fixed

--- src/read_csv_data.py
+++ src/read_csv_data.py
@@ -1,7 +1,7 @@
+"""System module"""
 import pandas as pd
 
+
 def read_csv(source):
+    """CSV file reading function"""
     return pd.read_csv(source)
-
-
-
--- tests/read_csv_data.py
+++ tests/read_csv_data.py
@@ -1,12 +1,15 @@
+"""System module"""
 import unittest
 import pandas as pd
 
 from src.read_csv_data import read_csv
 
-class readTest(unittest.TestCase):
+
+class TestForReadCsv(unittest.TestCase):
+    """the test class for reading csv files"""
     def test_for_csv(self):
-        self.assertIsInstance(read_csv('https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data'
-                                       '/main/Intelligent%20System%20Data/CSP/CSP_360.csv'), pd.DataFrame)
-
-
-
+        """test function for reading csv files"""
+        self.assertIsInstance(read_csv('https://raw.githubusercontent.com/EKU-Summer-2021/'
+                                       'intelligent_system_data''/main/'
+                                       'Intelligent%20System%20Data/CSP/'
+                                       'CSP_360.csv'), pd.DataFrame)
